194438aacaa79203cff0a46f3e997d9b2e2b0148
tyjeon <taeyongjeon24@gmail.com>
2022-03-21 00:59:29 +0900

    Add basic function code
    https://medium.com/naver-cloud-platform/%EC%9D%B4%EB%A0%87%EA%B2%8C-%EC%82%AC%EC%9A%A9%ED%95%98%EC%84%B8%EC%9A%94-clova-summary%EB%A1%9C-%EB%89%B4%EC%8A%A4-%EC%9A%94%EC%95%BD-%EC%84%9C%EB%B9%84%EC%8A%A4-%EB%A7%8C%EB%93%A4%EA%B8%B0-%EC%9D%B4%EA%B1%B4-%EB%A7%88%EC%B9%98-%EC%84%B8%EC%A4%84-%EC%9A%94%EC%95%BD-%EB%B4%87-dac29e97d1e4

---
+++ summarizer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import requests
+import json
+
+try:
+	with open("api_info.txt", "r") as f:
+		client_id = f.readline().replace("\n","")
+		client_secret = f.readline().replace("\n","")
+
+except FileNotFoundError:
+	with open("api_info.txt", "w") as f:
+		f.write("Replace this with your client_id(example: oawv4qurkm)\nReplace this with your client_secret(example: x2gds3M35LT419asdsadqtggZhSq6gBojKQwVY)")
+		print("You will need Naver API client. I'll make you a info file. Fill the file with content and try again.")
+
+
+
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/json"
+}
+language = "ko" # Language of document (ko, ja )
+model = "news" # Model used for summaries (general, news)
+tone = "2" # Converts the tone of the summarized result. (0, 1, 2, 3)
+summaryCount = "3" # This is the number of sentences for the summarized document.
+url= "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize" 
+title= "'하루 2000억' 판 커지는 간편송금 시장"
+content = "간편송금 이용금액이 하루 평균 2000억원을 넘어섰다. 한국은행이 17일 발표한 '2019년 상반기중 전자지급서비스 이용 현황'에 따르면 올해 상반기 간편송금서비스 이용금액(일평균)은 지난해 하반기 대비 60.7% 증가한 2005억원으로 집계됐다. 같은 기간 이용건수(일평균)는 34.8% 늘어난 218만건이었다. 간편 송금 시장에는 선불전자지급서비스를 제공하는 전자금융업자와 금융기관 등이 참여하고 있다. 이용금액은 전자금융업자가 하루평균 1879억원, 금융기관이 126억원이었다. 한은은 카카오페이, 토스 등 간편송금 서비스를 제공하는 업체 간 경쟁이 심화되면서 이용규모가 크게 확대됐다고 분석했다. 국회 정무위원회 소속 바른미래당 유의동 의원에 따르면 카카오페이, 토스 등 선불전자지급서비스 제공업체는 지난해 마케팅 비용으로 1000억원 이상을 지출했다. 마케팅 비용 지출규모는 카카오페이가 491억원, 비바리퍼블리카(토스)가 134억원 등 순으로 많았다."
+data = {
+    "document": {
+    "title": title,
+    "content" : content
+    },
+    "option": {
+    "language": language,
+    "model": model,
+    "tone": tone,
+    "summaryCount" : summaryCount
+    }
+}
+# print(json.dumps(data, indent=4, sort_keys=True))
+response = requests.post(url, data=json.dumps(data), headers=headers)
+rescode = response.status_code
+if(rescode == 200):
+    print (response.text)
+else:
+    print("Error : " + response.text)
