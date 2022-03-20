7b49901e1445a1c8cd8ae29367f6ac783415a785
tyjeon <taeyongjeon24@gmail.com>
2022-03-21 01:31:57 +0900

    Update summarizer.py
    Add TODO list in file
    Add news site parser

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
+from bs4 import BeautifulSoup
 import requests
+
+
 import json
+import re
+import sys
+
+PARSER = {
+	"electimes" : {
+		"title_selector" : "#article-view > div > header > div.heading-group > h3",
+		"content_selector" : "#article-view-content-div"
+	}
+}
+
+# TODO 1 : 리팩토링(클래스화)
+# TODO 3 : 전기신문 리스트에서 링크 뽑아오기 - > 그 링크에서 TODO 2에 해당하는 작업을 수행하는 거임
+# TODO 4 : 텔레그램봇 연결해서 보내기.
 
 try:
 	with open("api_info.txt", "r") as f:
@@ -26,8 +41,15 @@ model = "news" # Model used for summaries (general, news)
 tone = "2" # Converts the tone of the summarized result. (0, 1, 2, 3)
 summaryCount = "3" # This is the number of sentences for the summarized document.
 url= "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize" 
-title= "'하루 2000억' 판 커지는 간편송금 시장"
-content = "간편송금 이용금액이 하루 평균 2000억원을 넘어섰다. 한국은행이 17일 발표한 '2019년 상반기중 전자지급서비스 이용 현황'에 따르면 올해 상반기 간편송금서비스 이용금액(일평균)은 지난해 하반기 대비 60.7% 증가한 2005억원으로 집계됐다. 같은 기간 이용건수(일평균)는 34.8% 늘어난 218만건이었다. 간편 송금 시장에는 선불전자지급서비스를 제공하는 전자금융업자와 금융기관 등이 참여하고 있다. 이용금액은 전자금융업자가 하루평균 1879억원, 금융기관이 126억원이었다. 한은은 카카오페이, 토스 등 간편송금 서비스를 제공하는 업체 간 경쟁이 심화되면서 이용규모가 크게 확대됐다고 분석했다. 국회 정무위원회 소속 바른미래당 유의동 의원에 따르면 카카오페이, 토스 등 선불전자지급서비스 제공업체는 지난해 마케팅 비용으로 1000억원 이상을 지출했다. 마케팅 비용 지출규모는 카카오페이가 491억원, 비바리퍼블리카(토스)가 134억원 등 순으로 많았다."
+
+
+news_test_url = "https://www.electimes.com/news/articleView.html?idxno=302222"
+html = requests.get(news_test_url).text
+title = BeautifulSoup(html, "html.parser").select(PARSER["electimes"]["title_selector"])[0].text.strip()
+
+news_raw_text = BeautifulSoup(html, "html.parser").select(PARSER["electimes"]["content_selector"])[0].text.strip()
+content = re.sub("\n+"," ",news_raw_text)
+
 data = {
     "document": {
     "title": title,
@@ -40,7 +62,6 @@ data = {
     "summaryCount" : summaryCount
     }
 }
-# print(json.dumps(data, indent=4, sort_keys=True))
 response = requests.post(url, data=json.dumps(data), headers=headers)
 rescode = response.status_code
 if(rescode == 200):
